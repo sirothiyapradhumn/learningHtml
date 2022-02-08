e914336b62906efab89c27fc78bac435ad71ca81
Pradhumn Siorthiya <sirothiyapradhumn@gmail.com>
2022-02-08 23:18:27 +0530

    JS basic

---
+++ js/index.js
@@ -0,0 +1,50 @@
+// starting JS -> JavaScript
+// var, let, const
+
+// variable declaration
+var a ; // int, float, string
+
+// var initalization
+a = 10;
+
+console.log(a);
+
+a = "Hello"
+console.log(a);
+
+// typeof
+
+console.log(typeof a);
+
+// boollean type
+a = true;
+console.log(a);
+console.log(typeof a);
+
+a = null;
+console.log(a);
+console.log(typeof a);
+
+//number 
+var num = 10;
+console.log(num);
+var float = 2.5
+console.log(float);
+
+// boollean
+var t = true;
+var f = false;
+console.log(t);
+console.log(f);
+
+// String
+// ' ', " ", ``
+var str;
+str = "How are you Doing? \nYUP i am fine. How about you?"
+console.log(str);
+str1 = `STR1 How are you Doing? 
+YUP i am fine. How about you?`
+console.log(str1);
+
+var num = 100;
+console.log(`half of ${num} is ${num/2}`);
