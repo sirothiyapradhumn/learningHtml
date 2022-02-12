20ce93aaf19e74bb9876127183d5500293f1dd05
Pradhumn Siorthiya <sirothiyapradhumn@gmail.com>
2022-02-12 22:15:00 +0530

    let and var kai scope

--- js/index.js
+++ js/index.js
@@ -1,75 +1,89 @@
-// starting JS -> JavaScript
-// var, let, const
+// // starting JS -> JavaScript
+// // var, let, const
 
-// variable declaration
-var a ; // int, float, string
+// // variable declaration
+// var a ; // int, float, string
 
-// var initalization
-a = 10;
+// // var initalization
+// a = 10;
 
-console.log(a);
+// console.log(a);
 
-a = "Hello"
-console.log(a);
+// a = "Hello"
+// console.log(a);
 
-// typeof
+// // typeof
 
-console.log(typeof a);
+// console.log(typeof a);
 
-// boollean type
-a = true;
-console.log(a);
-console.log(typeof a);
+// // boollean type
+// a = true;
+// console.log(a);
+// console.log(typeof a);
 
-a = null;
-console.log(a);
-console.log(typeof a);
+// a = null;
+// console.log(a);
+// console.log(typeof a);
 
-//number 
-var num = 10;
-console.log(num);
-var float = 2.5
-console.log(float);
+// //number 
+// var num = 10;
+// console.log(num);
+// var float = 2.5
+// console.log(float);
 
-// boollean
-var t = true;
-var f = false;
-console.log(t);
-console.log(f);
+// // boollean
+// var t = true;
+// var f = false;
+// console.log(t);
+// console.log(f);
 
-// String
-// ' ', " ", ``
-// single quotes, double quotes, backticks
-var str;
-str = "How are you Doing? \nYUP i am fine. How about you?"
-console.log(str);
-str1 = `STR1 How are you Doing? 
-YUP i am fine. How about you?`
-console.log(str1);
+// // String
+// // ' ', " ", ``
+// // single quotes, double quotes, backticks
+// var str;
+// str = "How are you Doing? \nYUP i am fine. How about you?"
+// console.log(str);
+// str1 = `STR1 How are you Doing? 
+// YUP i am fine. How about you?`
+// console.log(str1);
 
-//backticks
-var num = 100;
-console.log(`half of ${num} is ${num/2}`);
+// //backticks
+// var num = 100;
+// console.log(`half of ${num} is ${num/2}`);
 
-let l = 100;
-console.log(l);
+// let l = 100;
+// console.log(l);
 
-//let l = 200; //SyntaxError: Identifier 'l' has already been declared
-//console.log(l);
+// //let l = 200; //SyntaxError: Identifier 'l' has already been declared
+// //console.log(l);
 
-l = 200;
-console.log(l);
+// l = 200;
+// console.log(l);
 
-// loop s in js
+// // loop s in js
 
-var num = 10;
-for(var i =1; i<=num; i++){
+// var num = 10;
+// for(var i =1; i<=num; i++){
+//     if(i % 2 == 0){
+//         console.log("number is even "+i);
+//     }
+// }
+
+// // const -> NO redeclare NO reinitialize
+// const num1 = 2;
+// console.log(num1);
+// //num1 = 4; //TypeError: Assignment to constant variable
+
+
+// DATE 12 FEB 2022
+
+for(var i = 1; i<10; i++){
+    let j = 20;
     if(i % 2 == 0){
-        console.log("number is even "+i);
+        console.log(i);
+        var hello = 1000;
     }
 }
-
-// const -> NO redeclare NO reinitialize
-const num1 = 2;
-console.log(num1);
-//num1 = 4; //TypeError: Assignment to constant variable
+console.log(hello);
+console.log(i);
+//console.log(j); // j is not defined
