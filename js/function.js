8511e03c7dd1ee6513f87f5f800a4ecd7bde8597
Pradhumn Siorthiya <sirothiyapradhumn@gmail.com>
2022-02-13 10:43:51 +0530

    update functions normal type & function expression

--- js/function.js
+++ js/function.js
@@ -16,16 +16,37 @@ function calculator (str, a, b){
             console.log(a+b);
         }
     }
+    if(str == 'sub'){
+        return function add (){
+            console.log(a-b);
+        }
+    }
+    if(str == 'mul'){
+        return function add (){
+            console.log(a*b);
+        }
+    }
+    if(str == 'div'){
+        return function add (){
+            console.log(a/b);
+        }
+    }
 }
 
-let returnfunc  = calculator('add', 2, 3);
+let returnfunc  = calculator('add', 5, 2);
+let returnfunc1 = calculator('sub', 5, 2);
+let returnfunc2 = calculator('mul', 5, 2);
+let returnfunc3 = calculator('div', 5, 2);
 console.log("Return function is \n"+ returnfunc);
 returnfunc();
+returnfunc1();
+returnfunc2();
+returnfunc3();
 
 
 //2nd type Function Expression
 
-let sayHi = function (){
+let sayHi = function (){   //  -> this is called as expression
     console.log("Hi i am function");
 }
 
