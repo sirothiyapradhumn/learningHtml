1cd6d51ced5f04fd0ba43e739d077bd4f9e2dc7e
Pradhumn Siorthiya <sirothiyapradhumn@gmail.com>
2022-02-13 10:30:10 +0530

    functions normal type & function expression

---
+++ js/function.js
@@ -0,0 +1,43 @@
+//function defination
+/*
+function add(a, b){
+    console.log(a+b);
+}
+
+//function revoke
+add(3,5);
+
+*/
+
+function calculator (str, a, b){
+    if(str == 'add'){
+        return function add (){
+            console.log(a+b);
+        }
+    }
+}
+
+let returnfunc  = calculator('add', 2, 3);
+console.log("Return function is \n"+ returnfunc);
+returnfunc();
+
+
+//2nd type Function Expresiion
+
+let sayHi = function (){
+    console.log("Hi i am function");
+}
+
+console.log("line no. 31 \n"+sayHi);
+
+sayHi(); // function invoking
+
+/* o/p
+
+line no. 31
+function (){
+    console.log("Hi i am function");
+}
+Hi i am function
+
+*/
