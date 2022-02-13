4ce4646c5ef15a3e424b5eab00f9cb7fef5666dc
Pradhumn Siorthiya <sirothiyapradhumn@gmail.com>
2022-02-13 10:59:57 +0530

    3 type function IIFE

--- js/function.js
+++ js/function.js
@@ -10,49 +10,49 @@ add(3,5);
 
 */
 
-function calculator (str, a, b){
-    if(str == 'add'){
-        return function add (){
-            console.log(a+b);
-        }
-    }
-    if(str == 'sub'){
-        return function add (){
-            console.log(a-b);
-        }
-    }
-    if(str == 'mul'){
-        return function add (){
-            console.log(a*b);
-        }
-    }
-    if(str == 'div'){
-        return function add (){
-            console.log(a/b);
-        }
-    }
-}
+// function calculator (str, a, b){
+//     if(str == 'add'){
+//         return function add (){
+//             console.log(a+b);
+//         }
+//     }
+//     if(str == 'sub'){
+//         return function add (){
+//             console.log(a-b);
+//         }
+//     }
+//     if(str == 'mul'){
+//         return function add (){
+//             console.log(a*b);
+//         }
+//     }
+//     if(str == 'div'){
+//         return function add (){
+//             console.log(a/b);
+//         }
+//     }
+// }
 
-let returnfunc  = calculator('add', 5, 2);
-let returnfunc1 = calculator('sub', 5, 2);
-let returnfunc2 = calculator('mul', 5, 2);
-let returnfunc3 = calculator('div', 5, 2);
-console.log("Return function is \n"+ returnfunc);
-returnfunc();
-returnfunc1();
-returnfunc2();
-returnfunc3();
+// let returnfunc  = calculator('add', 5, 2);
+// let returnfunc1 = calculator('sub', 5, 2);
+// let returnfunc2 = calculator('mul', 5, 2);
+// let returnfunc3 = calculator('div', 5, 2);
+// console.log("Return function is \n"+ returnfunc);
+// returnfunc();
+// returnfunc1();
+// returnfunc2();
+// returnfunc3();
 
 
 //2nd type Function Expression
 
-let sayHi = function (){   //  -> this is called as expression
-    console.log("Hi i am function");
-}
+// let sayHi = function (){   //  -> this is called as expression
+//     console.log("Hi i am function");
+// }
 
-console.log("line no. 31 \n"+sayHi);
+// console.log("line no. 31 \n"+sayHi);
 
-sayHi(); // function invoking
+// sayHi(); // function invoking
 
 /* o/p
 
@@ -63,3 +63,9 @@ function (){
 Hi i am function
 
 */
+
+// 3rd type function IIFE -> imediately invoke function expression
+
+let addIIFE = (function add(a,b){
+    console.log(a+b);
+})(20, 30);
