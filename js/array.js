77e69e2327456299e0fd50061a64d5c1e84e0247
Pradhumn Siorthiya <sirothiyapradhumn@gmail.com>
2022-02-13 21:56:42 +0530

    array & 2dArray complete

--- js/array.js
+++ js/array.js
@@ -1,7 +1,7 @@
 
 // array -> is collection of elements
 
-let cars = ['BMW', 'AUDI', 'MG', 1, 3 ,4 , true];
+let cars = ['BMW', "AUDI", 'MG', 1, 3 ,4 , true];
 console.log(cars);
 
 // Accessing the elemets from array
@@ -24,40 +24,39 @@ console.log(cars);
 
 
 // methods in array
- // -> pop method -> last element pop
+// -> pop method -> last element pop
 
- cars.pop();
- console.log(cars);   // o/p [ 'BMW', 'AUDI', 'MG', 1, 'Mahindra', 4, true ]
+cars.pop();
+console.log(cars);   // o/p [ 'BMW', 'AUDI', 'MG', 1, 'Mahindra', 4, true ]
 
- // push -> push elemnet in last element of array
+// push -> push elemnet in last element of array
 
- cars.push("123");
- console.log(cars); // o/p [ 'BMW', 'AUDI', 'MG', 1, 'Mahindra', 4, true, '123' ]
+cars.push("123");
+console.log(cars); // o/p [ 'BMW', 'AUDI', 'MG', 1, 'Mahindra', 4, true, '123' ]
 
- // unshift method -> it add element at the staring of array
+// unshift method -> it add element at the staring of array
 
- cars.unshift('Tempo'); 
- console.log(cars); //o/p   [ 'Tempo', 'BMW', 'AUDI', 'MG', 1, 'Mahindra', 4, true, '123' ]
+cars.unshift('Tempo'); 
+console.log(cars); //o/p   [ 'Tempo', 'BMW', 'AUDI', 'MG', 1, 'Mahindra', 4, true, '123' ]
 
- // shift -> it remove elemnt from 0th element
+// shift -> it remove elemnt from 0th element
 
- cars.shift();
- cars.shift();
- console.log(cars); // o/p [ 'AUDI', 'MG', 1, 'Mahindra', 4, true, '123' ]
+cars.shift();
+cars.shift();
+console.log(cars); // o/p [ 'AUDI', 'MG', 1, 'Mahindra', 4, true, '123' ]
 
 
+/* 2D ARRAY */
+let array2d = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9]
+];
 
- /* 2D ARRAY */
+console.log("2D array");
+console.log(array2d);
 
- let array2d = [
-     [1, 2, 3],
-     [4, 5, 6],
-     [7, 8, 9]
- ];
+console.table(array2d); // print 2d array in form of table
 
- console.log("2D array");
- console.log(array2d);
-
- console.table(array2d);
-
- console.log(array2d[1][2]);
+console.log(array2d[1][2]);
+ 
