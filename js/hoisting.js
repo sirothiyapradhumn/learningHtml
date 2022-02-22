66bdf1447a8c42f0909435580697a572b3f05f21
Pradhumn Siorthiya <sirothiyapradhumn@gmail.com>
2022-02-22 22:28:00 +0530

    Hoisting in js, code debug in chrome

---
+++ js/hoisting.js
@@ -0,0 +1,24 @@
+/*
+var  x = 10;
+
+function greet(){
+    console.log("hello Sahil");
+}
+
+console.log(x);
+greet();
+
+// o/p   10 --- hello sahil
+
+*/
+
+
+console.log(x);
+greet();
+var  x = 10;
+
+function greet(){
+    console.log("hello Sahil");
+}
+
+// undefined --- hello Sahil
