636314e1c9e76dda8650e9209e6495bef521febf
Pradhumn Siorthiya <sirothiyapradhumn@gmail.com>
2022-02-22 23:02:17 +0530

    update Hoisting

--- js/hoisting.js
+++ js/hoisting.js
@@ -15,10 +15,17 @@ greet();
 
 console.log(x);
 greet();
+hello(); //  error dega because   var hello : undefine, hai abhi and we are calling it as function
 var  x = 10;
 
+// function 
 function greet(){
     console.log("hello Sahil");
+}
+
+// function Expresion 
+var hello = function () {
+    console.log("i am error");
 }
 
 // undefined --- hello Sahil
