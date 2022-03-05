8a90ebf604bb649403acb383921001d3aaaf47d4
Pradhumn Siorthiya <sirothiyapradhumn@gmail.com>
2022-03-05 21:47:13 +0530

    update closure, test

--- js/closure.js
+++ js/closure.js
@@ -1,12 +1,26 @@
+// function outter(){
+//     var a = 10;
+//     function inner (){
+//         console.log(a);
+//     }
+//     return inner;
+// }
+
+// var z = outter();
+// console.log(z+" ");
+
+// z();
+
 function outter(){
     var a = 10;
     function inner (){
         console.log(a);
     }
+    a = 7;
     return inner;
 }
 
 var z = outter();
 console.log(z+" ");
 
-z();
+z(); // o/p 7
