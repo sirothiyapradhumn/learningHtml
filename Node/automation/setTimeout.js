923fcefdd3da95c97572ad10f807120df95d894c
Pradhumn Siorthiya <sirothiyapradhumn@gmail.com>
2022-04-13 00:13:02 +0530

    print 1 to 10 with 2 sec wait

--- Node/automation/setTimeout.js
+++ Node/automation/setTimeout.js
@@ -1,25 +1,31 @@
-{
-    function yolo() {
-        var a = 10;
-        function cb(err, res) {
-            if (err) {
-                console.log(err);
-            }
-            else {
-                console.log("hello how are you ?");
+
+    // function yolo() {
+    //     var a = 10;
+    //     function cb(err, res) {
+    //         if (err) {
+    //             console.log(err);
+    //         }
+    //         else {
+    //             console.log("hello how are you ?");
                 
-            }
-        }
-        // function to be executed(callback fun) , time(ms) after which cb to be executed
-        // 1 second =1000 milli second
-        setTimeout(cb, 5000);
-        console.log(a);
+    //         }
+    //     }
+    //     // function to be executed(callback fun) , time(ms) after which cb to be executed
+    //     // 1 second =1000 milli second
+    //     setTimeout(cb, 5000);
+    //     console.log(a);
     
-    }
+    // }
     
-    yolo();
-    let b = 100;
-    console.log(b);
+    // yolo();
+    // let b = 100;
+    // console.log(b);
+    
+    for (let i = 1; i <= 10; i++){
+        // hint: closures and scope -> wrapping
+        setTimeout(function () {
+            console.log(i);
+        }, 2000*i);
     }
     
     
